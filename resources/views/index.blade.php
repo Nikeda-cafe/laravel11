151c93ebf296fa1f6348c4b0c4600fab36e3d410
nikeda <4104cafeaulait@gmail.com>
2026-01-25 17:27:16 +0900

    fix: deploy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Laravel Rolling Deployment</title>
+    <title>Welcome to Laravel Rolling Deployment 2</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         :root {
